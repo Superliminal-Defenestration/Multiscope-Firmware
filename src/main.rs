8343dcd8334090b2460009698584a067b409e63d
Pengalu <pengalu99@gmail.com>
2025-10-31 14:44:16 -0700

    HSE Setup + I/O

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,36 @@ use panic_halt as _;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal::{
-    pac,
-    prelude::*,
+    gpio::{self, Edge, gpiob, gpioc}, pac::{self, rcc::cfgr}, prelude::*, rcc::Config
 };
 
 #[allow(clippy::empty_loop)]
 #[entry]
 fn main() -> ! {
-    loop {}
+
+    let mut p=pac::Peripherals::take().unwrap();
+    let mut cp =  cortex_m::peripheral::Peripherals::take().unwrap();
+    
+    let mut rcc = p.RCC.constrain();
+  
+    let mut syscfg = p.SYSCFG.constrain(&mut rcc);
+    
+    let gpiob = p.GPIOB.split(&mut rcc);  
+    let mut keyLeft = gpiob.pb10.into_pull_down_input();
+
+    let clocks = rcc.freeze(Config::hse(8.MHz()).sysclk(48.MHz())); // the "freeze" method applies a configuration to the clock config registry
+    // the clock is configured for an 8MHz external oscilator on the OSC pins, caled the HSE. This is chained with a sysclk of 48MHz, which will be achieved through internal multiplication of thre clock's signal.
+    
+
+ 
+
+    loop {
+
+        if keyLeft.is_high() {
+
+            
+
+        }
+
+    }
 }
