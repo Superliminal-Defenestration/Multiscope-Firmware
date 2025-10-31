5504e7c85a66c02d06060130ea6ad144c6acd8a6
Pengalu <pengalu99@gmail.com>
2025-10-31 14:47:21 -0700

    more comments

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,17 @@ use stm32f4xx_hal::{
 #[entry]
 fn main() -> ! {
 
-    let mut p=pac::Peripherals::take().unwrap();
-    let mut cp =  cortex_m::peripheral::Peripherals::take().unwrap();
+    let mut p=pac::Peripherals::take().unwrap(); // Access the peripherals through the PAC
+    let mut cp =  cortex_m::peripheral::Peripherals::take().unwrap(); // Access peripherals through hardware
     
-    let mut rcc = p.RCC.constrain();
+    let mut rcc = p.RCC.constrain(); // Access the Reset + Clock Controller, and constrain it. 
+
+    //ALl peripherals are either constrained or split. Split splits into individual registries with full access. Constrain provides a restricted, hardware-safe view.
   
-    let mut syscfg = p.SYSCFG.constrain(&mut rcc);
+    let mut syscfg = p.SYSCFG.constrain(&mut rcc); // Access the syscfg and constrain it!
     
-    let gpiob = p.GPIOB.split(&mut rcc);  
-    let mut keyLeft = gpiob.pb10.into_pull_down_input();
+    let gpiob = p.GPIOB.split(&mut rcc);   // GPIO-B port is a low-risk peripheral to access, so we'll just split it.
+    let mut keyLeft = gpiob.pb10.into_pull_down_input(); // The diagram has this as a pull-down'd input
 
     let clocks = rcc.freeze(Config::hse(8.MHz()).sysclk(48.MHz())); // the "freeze" method applies a configuration to the clock config registry
     // the clock is configured for an 8MHz external oscilator on the OSC pins, caled the HSE. This is chained with a sysclk of 48MHz, which will be achieved through internal multiplication of thre clock's signal.
