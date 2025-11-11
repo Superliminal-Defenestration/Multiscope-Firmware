e1483e1ef3321ba7c2fa412b3f35a83db0e10dc0
Pengalu <pengalu99@gmail.com>
2025-11-11 09:03:31 -0800

    created new branch

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use panic_halt as _;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal::{
-    adc::{self, Adc, config::AdcConfig}, gpio::{self, Edge, PB6, PB10, PB11, gpiob, gpioc}, nb, pac::{self, ADC1, UART4, USART1, USART3, adc1, rcc::cfgr}, prelude::*, rcc::Config as RccConfig, serial::{self, Serial, config::Config}
+    adc::{self, Adc, config::AdcConfig}, gpio::{self, AF5, AF15, Alternate, Edge, PB6, PB10, PB11, Pin, alt::fmc::A5, gpiob, gpioc}, hal::spi, nb, pac::{self, ADC1, UART4, USART1, USART3, adc1, dcmi::mis, rcc::cfgr}, prelude::*, rcc::Config as RccConfig, serial::{self, Serial, config::Config}, spi::{Mode, Phase, Polarity, Spi}, time::Hertz
 };
 use core::fmt::Write; // for pretty formatting of the serial output
 #[allow(clippy::empty_loop)]
@@ -27,13 +27,25 @@ fn main() -> ! {
     
     let gpiob = p.GPIOB.split(&mut rcc);   // GPIO-B port is a low-risk peripheral to access, so we'll just split it.
     let gpioc = p.GPIOC.split(&mut rcc);   // GPIO-B port is a low-risk peripheral to access, so we'll just split it.
-    
+    let gpioa = p.GPIOA.split(&mut rcc);
+
+    //init SPI bus
+    let mut  sck = gpioa.pa5.into_alternate::<5>() ; // We have to tell the pin to register as an SPI pin instead of GPIO
+    // AF 5 (Alt Func 5) is the func register for SPI
+    let mut mosi: Pin<'A', 7, Alternate<_>> = gpioa.pa7.into_alternate::<5>() ; // We have to tell the pin to register as an SPI pin instead of GPIO
+    let  mut miso: Pin<'A', 6, Alternate<_>> = gpioa.pa6.into_alternate::<5>() ; // We have to tell the pin to register as an SPI pin instead of GPIO
+    let spi_mode = Mode {
+        polarity: Polarity::IdleLow,
+        phase: Phase::CaptureOnFirstTransition,
+    };
+    let mut spiBus = Spi::new(p.SPI1, (core::prelude::v1::Some(sck),core::prelude::v1::Some(miso),core::prelude::v1::Some(mosi)), spi_mode,Hertz::MHz(8), &mut rcc);
+
     let mut keyLeft = gpiob.pb10.into_pull_down_input(); // The diagram has this as a pull-down'd input
 
     let mut clocks: stm32f4xx_hal::rcc::Rcc = rcc.freeze(RccConfig::hse(8.MHz()).sysclk(48.MHz())); // the "freeze" method applies a configuration to the clock config registry
     // the clock is configured for an 8MHz external oscilator on the OSC pins, caled the HSE. This is chained with a sysclk of 48MHz, which will be achieved through internal multiplication of thre clock's signal.
     let mut serial: Serial<UART4> = Serial::new(p.UART4, (gpioc.pc10,gpioc.pc11), Config::default().baudrate(3200.bps()), &mut clocks).unwrap(); // value is moved to clocks.
-
+    
 
     writeln!(serial, "about to instance adc").ok();
 
@@ -73,8 +85,8 @@ fn main() -> ! {
             // echo the received byte back to the sender
             nb::block!(serial.write(byte)).ok();
         }
-
-
+        let spi_res = spiBus.write(b"hey").ok();
+        
         // let adc_result: Result<u16, nb::Error<()>>= (adcDriver0.read( &mut p0));
 
         if keyLeft.is_high() {
